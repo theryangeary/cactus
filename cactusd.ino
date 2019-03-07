70c7dab3a6c8d73dcb3a5b2c60388eda9ef27962
Ryan Geary <rtgnj42@gmail.com>
2019-03-07 15:43:10 -0500

    Make blinking LED proof-of-concept program

--- cactusd.ino
+++ cactusd.ino
@@ -1,11 +1,16 @@
 #include "libraries/Common.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED 2
 
+void setup() {
+  // initialize digital pin LED as an output.
+  pinMode(LED, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
