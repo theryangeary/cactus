181db870537a88d06839b7cdb9dc2ced92499851
Ryan Geary <rtgnj42@gmail.com>
2019-03-11 10:14:56 -0400

    Show preprocessor directives and serial communication

--- cactusd.ino
+++ cactusd.ino
@@ -5,10 +5,17 @@
 void setup() {
   // initialize digital pin LED as an output.
   pinMode(LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+#ifdef SERVER
+  Serial.println("Anna");
+#endif
+#ifdef CLIENT
+  Serial.println("Ryan");
+#endif
   digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
