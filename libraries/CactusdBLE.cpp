8e4122a2804734a5f90aa194d24555707745e3d1
Ryan Geary <rtgnj42@gmail.com>
2019-03-20 11:19:04 -0400

    Add library for BLE functionality

---
+++ libraries/CactusdBLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Bluetooth Low Energy library for cactusd
+ */
+
+#include "CactusdBLE.h"
+
+void bleTest() {
+  Serial.println("ble included");
+  return;
+}
---
+++ libraries/CactusdBLE.h
@@ -0,0 +1,8 @@
+/*
+ * Bluetooth Low Energy library for cactusd
+ */
+
+#include "Arduino.h"
+
+void bleTest();
+
